a9c4b361492cbed370e45fc16a7ef29e0f4a56b9
Gayathri Soundararajan <sgaya10z@gmail.com>
2020-08-28 18:22:27 +0400

    .....

--- chat_poc/lib/models/ChatModel.dart
+++ chat_poc/lib/models/ChatModel.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:convert';
 
 import 'package:chat_poc/models/Examples.dart';
@@ -12,7 +13,7 @@ import 'package:scoped_model/scoped_model.dart';
 import './User.dart';
 import './Message.dart';
 
-class ChatModel extends Model {
+class ChatModel extends ChangeNotifier {
   // IO.Socket socket;
   SocketIO socketIO;
 
@@ -26,40 +27,18 @@ class ChatModel extends Model {
 
   Group group = Examples.group;
 
-  static ChatModel of(BuildContext context) =>
-      ScopedModel.of<ChatModel>(context, rebuildOnChange: true);
+  List<Message> get getMessages => UnmodifiableListView(messages);
 
-  void init() {
+  void init() async {
     currentUser = users[1]; // run as 1 2 3 in different devices
 
     print('CURRENT USER: ${currentUser.userID}');
-
-    // contacts =
-    //     users.where((user) => user.userID != currentUser.userID).toList();
-
-    // socket =
-    //     IO.io('https://calm-savannah-01592.herokuapp.com', <String, dynamic>{
-    //   'transports': ['websocket'],
-    //   'query': 'roomID=${currentUser.userID}'),
-    // });
 
     socketIO = SocketIOManager().createSocketIO(
         'https://calm-savannah-01592.herokuapp.com', '/',
         query: 'roomID=${currentUser.userID}');
 
     socketIO.init();
-
-    // socket.on("receive_message", (response) {
-    //   Map<String, dynamic> data = json.decode(response);
-    //   messages.add(Message(
-    //     content: data["content"],
-    //     senderID: data["senderChatID"],
-    //     recipientID: data["receiverChatID"],
-    //     time: DateFormat.jm().format(DateTime.now()),
-    //   ));
-
-    // notifyListeners();
-    // });
 
     socketIO.subscribe('receive_message', (jsonData) {
       Map<String, dynamic> data = json.decode(jsonData);
@@ -70,17 +49,11 @@ class ChatModel extends Model {
         recipientID: data["receiverChatID"],
         time: DateFormat.jm().format(DateTime.now()),
       ));
-      print(messages);
-
+      print(messages.length);
       notifyListeners();
     });
 
-    // socket.connect();
     socketIO.connect();
-
-    // socket.on('connect', (data) {
-    //   print("Connection Initiated: $data");
-    // });
   }
 
   void sendMessage(String text, String recipientID) {
@@ -91,15 +64,6 @@ class ChatModel extends Model {
       isGroup: false, //individual message change  to true when testng for group
       time: DateFormat.jm().format(DateTime.now()),
     ));
-
-    // socket.emit(
-    //     "send_message",
-    //     json.encode({
-    //       'receiverChatID': recipientID,
-    //       'senderChatID': currentUser.userID,
-    //       'content': text,
-    //       'time': DateFormat.jm().format(DateTime.now()),
-    //     }));
 
     socketIO.sendMessage(
       'send_message',
@@ -112,6 +76,7 @@ class ChatModel extends Model {
             false, //individual message change  to true when testng for group
       }),
     );
+    print(messages.length);
 
     notifyListeners();
   }
@@ -124,3 +89,116 @@ class ChatModel extends Model {
         .toList();
   }
 }
+
+// class ChatModel extends Model {
+//   // IO.Socket socket;
+//   SocketIO socketIO;
+
+//   List<User> users = Examples.users;
+
+//   User currentUser;
+//   List<User> contacts = List<User>();
+
+//   // List<Message> messages = List<Message>();
+//   List<Message> messages = Examples.messages;
+
+//   Group group = Examples.group;
+
+//   static ChatModel of(BuildContext context) =>
+//       ScopedModel.of<ChatModel>(context, rebuildOnChange: true);
+
+//   void init() {
+//     currentUser = users[1]; // run as 1 2 3 in different devices
+
+//     print('CURRENT USER: ${currentUser.userID}');
+
+//     // contacts =
+//     //     users.where((user) => user.userID != currentUser.userID).toList();
+
+//     // socket =
+//     //     IO.io('https://calm-savannah-01592.herokuapp.com', <String, dynamic>{
+//     //   'transports': ['websocket'],
+//     //   'query': 'roomID=${currentUser.userID}'),
+//     // });
+
+//     socketIO = SocketIOManager().createSocketIO(
+//         'https://calm-savannah-01592.herokuapp.com', '/',
+//         query: 'roomID=${currentUser.userID}');
+
+//     socketIO.init();
+
+//     // socket.on("receive_message", (response) {
+//     //   Map<String, dynamic> data = json.decode(response);
+//     //   messages.add(Message(
+//     //     content: data["content"],
+//     //     senderID: data["senderChatID"],
+//     //     recipientID: data["receiverChatID"],
+//     //     time: DateFormat.jm().format(DateTime.now()),
+//     //   ));
+
+//     // notifyListeners();
+//     // });
+
+//     socketIO.subscribe('receive_message', (jsonData) {
+//       Map<String, dynamic> data = json.decode(jsonData);
+//       print(data);
+//       messages.add(Message(
+//         content: data["content"],
+//         senderID: data["senderChatID"],
+//         recipientID: data["receiverChatID"],
+//         time: DateFormat.jm().format(DateTime.now()),
+//       ));
+//       print(messages);
+
+//       notifyListeners();
+//     });
+
+//     // socket.connect();
+//     socketIO.connect();
+
+//     // socket.on('connect', (data) {
+//     //   print("Connection Initiated: $data");
+//     // });
+//   }
+
+//   void sendMessage(String text, String recipientID) {
+//     messages.add(Message(
+//       content: text,
+//       senderID: currentUser.userID,
+//       recipientID: recipientID,
+//       isGroup: false, //individual message change  to true when testng for group
+//       time: DateFormat.jm().format(DateTime.now()),
+//     ));
+
+//     // socket.emit(
+//     //     "send_message",
+//     //     json.encode({
+//     //       'receiverChatID': recipientID,
+//     //       'senderChatID': currentUser.userID,
+//     //       'content': text,
+//     //       'time': DateFormat.jm().format(DateTime.now()),
+//     //     }));
+
+//     socketIO.sendMessage(
+//       'send_message',
+//       json.encode({
+//         'receiverChatID': recipientID,
+//         'senderChatID': currentUser.userID,
+//         'content': text,
+//         'time': DateFormat.jm().format(DateTime.now()),
+//         'isGroup':
+//             false, //individual message change  to true when testng for group
+//       }),
+//     );
+
+//     notifyListeners();
+//   }
+
+//   List<Message> getMessagesForID(String chatID) {
+//     return messages
+//         .where((msg) =>
+//             msg.isGroup == false && //individual
+//             (msg.senderID == chatID || msg.recipientID == chatID))
+//         .toList();
+//   }
+// }
--- chat_poc/lib/pages/MainPage.dart
+++ chat_poc/lib/pages/MainPage.dart
@@ -1,14 +1,9 @@
-import 'dart:io';
-
-import 'package:chat_poc/models/ChatModel.dart';
 import 'package:flutter/material.dart';
 
 import 'package:chat_poc/components/CustomTabBar.dart';
 import 'package:chat_poc/components/CustomBottomBar.dart';
 import 'package:chat_poc/components/CustomAppBar.dart';
 import 'package:chat_poc/models/Examples.dart';
-import 'package:chat_poc/models/Group.dart';
-import 'package:chat_poc/models/User.dart';
 import 'package:chat_poc/pages/ChatPage.dart';
 import 'package:chat_poc/pages/ChatList.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -35,17 +30,8 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
     _tabController.addListener(() {
       print('Tab Controller index ${_tabController.index}');
     });
-    // category = ModalRoute.of(context).settings.arguments;
-    ScopedModel.of<ChatModel>(context, rebuildOnChange: false).init();
-    // ChatModel.of(context).init();
     super.initState();
   }
-
-  // @override
-  // void didChangeDependencies() {
-  //   super.didChangeDependencies();
-  //   ScopedModel.of<ChatModel>(context, rebuildOnChange: true).init();
-  // }
 
   @override
   void dispose() {
@@ -55,46 +41,128 @@ class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<ChatModel>(builder: (context, child, model) {
-      // print(model.currentUser);
-      // var model = ChatModel.of(context);
-
-      return Scaffold(
-        bottomNavigationBar: CustomBottomBar(),
-        appBar: PreferredSize(
-          preferredSize:
-              Size(double.infinity, MediaQuery.of(context).size.height / 6.7),
-          child: AppBar(
-            backgroundColor: const Color(0xff0f4d83),
-            leading: Container(),
-            elevation: 0,
-            flexibleSpace: Column(
-              children: [
-                CustomAppBar(
-                  title: Examples.directory[category]["title"],
-                  subtitle: Examples.directory[category]["subtitle"],
-                ),
-                CustomTabBar(
-                  tabs: Examples.myTabs,
-                  controller: _tabController,
-                ),
-              ],
-            ),
+    // return Consumer<ChatModel>(builder: (context, model, child) {
+    return Scaffold(
+      bottomNavigationBar: CustomBottomBar(),
+      appBar: PreferredSize(
+        preferredSize:
+            Size(double.infinity, MediaQuery.of(context).size.height / 6.7),
+        child: AppBar(
+          backgroundColor: const Color(0xff0f4d83),
+          leading: Container(),
+          elevation: 0,
+          flexibleSpace: Column(
+            children: [
+              CustomAppBar(
+                title: Examples.directory[category]["title"],
+                subtitle: Examples.directory[category]["subtitle"],
+              ),
+              CustomTabBar(
+                tabs: Examples.myTabs,
+                controller: _tabController,
+              ),
+            ],
           ),
         ),
-        body: TabBarView(
-          children: [
-            ChatList(category: category),
-            ChatPage(
-              // group: Group(name: 'Hostels', groupID: "999"),
-              contact1: Examples.users[1],
-              contact2: Examples.users[0],
-            ),
-            Center(child: Text('This is the Plugins tab')),
-          ],
-          controller: _tabController,
-        ),
-      );
-    });
+      ),
+      body: TabBarView(
+        children: [
+          ChatList(category: category),
+          ChatPage(
+            // group: Group(name: 'Hostels', groupID: "999"),
+            contact1: Examples.users[1],
+            contact2: Examples.users[0],
+          ),
+          Center(child: Text('This is the Plugins tab')),
+        ],
+        controller: _tabController,
+      ),
+    );
+    // });
   }
+
+// class MainPage extends StatefulWidget {
+//   final String category;
+
+//   MainPage({this.category});
+
+//   @override
+//   _MainPageState createState() => _MainPageState(category: category);
+// }
+
+// class _MainPageState extends State<MainPage> with TickerProviderStateMixin {
+//   TabController _tabController;
+//   String category;
+
+//   _MainPageState({this.category});
+
+//   @override
+//   void initState() {
+//     print('Category $category');
+//     _tabController = TabController(length: Examples.myTabs.length, vsync: this);
+//     _tabController.addListener(() {
+//       print('Tab Controller index ${_tabController.index}');
+//     });
+//     // category = ModalRoute.of(context).settings.arguments;
+//     ScopedModel.of<ChatModel>(context, rebuildOnChange: false).init();
+//     // ChatModel.of(context).init();
+//     super.initState();
+//   }
+
+//   // @override
+//   // void didChangeDependencies() {
+//   //   super.didChangeDependencies();
+//   //   ScopedModel.of<ChatModel>(context, rebuildOnChange: true).init();
+//   // }
+
+//   @override
+//   void dispose() {
+//     _tabController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScopedModelDescendant<ChatModel>(builder: (context, child, model) {
+//       // print(model.currentUser);
+//       // var model = ChatModel.of(context);
+
+//       return Scaffold(
+//         bottomNavigationBar: CustomBottomBar(),
+//         appBar: PreferredSize(
+//           preferredSize:
+//               Size(double.infinity, MediaQuery.of(context).size.height / 6.7),
+//           child: AppBar(
+//             backgroundColor: const Color(0xff0f4d83),
+//             leading: Container(),
+//             elevation: 0,
+//             flexibleSpace: Column(
+//               children: [
+//                 CustomAppBar(
+//                   title: Examples.directory[category]["title"],
+//                   subtitle: Examples.directory[category]["subtitle"],
+//                 ),
+//                 CustomTabBar(
+//                   tabs: Examples.myTabs,
+//                   controller: _tabController,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//         body: TabBarView(
+//           children: [
+//             ChatList(category: category),
+//             ChatPage(
+//               // group: Group(name: 'Hostels', groupID: "999"),
+//               contact1: Examples.users[1],
+//               contact2: Examples.users[0],
+//             ),
+//             Center(child: Text('This is the Plugins tab')),
+//           ],
+//           controller: _tabController,
+//         ),
+//       );
+//     });
+//   }
 }
